8fce402a11eb6e746ca6235856a7f45d19fea2b3
Neo Lee <neo.lee@gmail.com>
2022-12-02 16:00:41 +0800

    bump: all depends

--- util/holiday/project.clj
+++ util/holiday/project.clj
@@ -6,9 +6,9 @@
                  [org.clojure/data.json "2.4.0"]
                  [clj-http "3.12.3"]
                  [tick "0.5.0"]
-                 [org.mariadb.jdbc/mariadb-java-client "3.0.8"]
-                 [mysql/mysql-connector-java "8.0.30"]
-                 [com.github.seancorfield/honeysql "2.3.928"]
-                 [com.github.seancorfield/next.jdbc "1.3.834"]]
+                 [org.mariadb.jdbc/mariadb-java-client "3.1.0"]
+                 [mysql/mysql-connector-java "8.0.31"]
+                 [com.github.seancorfield/honeysql "2.4.947"]
+                 [com.github.seancorfield/next.jdbc "1.3.847"]]
   :repl-options {:init-ns net.paradigmx.util.holiday}
   :main ^{:skip-aot true} net.paradigmx.util.holiday)
