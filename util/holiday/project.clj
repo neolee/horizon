005944f7d34ea8afaf44e607479bc2531835fafc
Neo Lee <neo.lee@gmail.com>
2023-03-24 17:19:50 +0800

    bump: utility deps

--- util/holiday/project.clj
+++ util/holiday/project.clj
@@ -5,10 +5,10 @@
                  [com.github.clj-easy/graal-build-time "0.1.4"]
                  [org.clojure/data.json "2.4.0"]
                  [clj-http "3.12.3"]
-                 [tick "0.5.0"]
-                 [org.mariadb.jdbc/mariadb-java-client "3.1.1"]
-                 [mysql/mysql-connector-java "8.0.31"]
-                 [com.github.seancorfield/honeysql "2.4.969"]
-                 [com.github.seancorfield/next.jdbc "1.3.847"]]
+                 [tick "0.6.0"]
+                 [org.mariadb.jdbc/mariadb-java-client "3.1.3"]
+                 [mysql/mysql-connector-java "8.0.32"]
+                 [com.github.seancorfield/honeysql "2.4.1011"]
+                 [com.github.seancorfield/next.jdbc "1.3.862"]]
   :repl-options {:init-ns net.paradigmx.util.holiday}
   :main ^{:skip-aot true} net.paradigmx.util.holiday)
--- util/play24/project.clj
+++ util/play24/project.clj
@@ -3,6 +3,6 @@
   :url "https://soulhacker.me"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.github.clj-easy/graal-build-time "0.1.4"]
-                 [org.clojure/math.combinatorics "0.1.6"]]
+                 [org.clojure/math.combinatorics "0.2.0"]]
   :repl-options {:init-ns net.paradigmx.horizon.play24}
   :main ^{:skip-aot true} net.paradigmx.horizon.play24)
